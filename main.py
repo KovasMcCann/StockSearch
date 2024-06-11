ba91dc3f8ae93738defb2d69b8691084f9c5fca4
Kovas McCann <Kovasmccann@outlook.com>
2024-06-11 10:02:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import requests
 
 def getprice(symbol):
   import requests
-  # replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
-  url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo'
+  url = f"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={symbol}&interval=5min&apikey=ZN9F4XFJ9CHZYZ44"
   r = requests.get(url)
   data = r.json()
   print(data)
