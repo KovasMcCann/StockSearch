e3019973d3cb7affd035dc65a770dc1271134c23
KovasMcCann <KovasMcCann@outlook.com>
2024-06-19 12:43:25 -0500

    added class

--- main.py
+++ main.py
@@ -1,63 +1,58 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 #header for SEC access
 
 usr_agnt = {'User-Agent' : 'Kovas McCann (KovasMcCann@outlook.com)'}
 Polygon_API_KEY = ''
 search_url = "https://www.sec.gov/cgi-bin/browse-edgar"
 
-def getprice(symbol):
-  print("hello")
-  #use polygon.io
 
-#Available form types [0-Q, 10-K,8-K, 20-F, 40-F, 6-K]
-#SEC implementation
-
-def Get_CIK(company_name):
-  params = {
-    'action': 'getcompany',
-    'company': company_name,
-    'owner': 'exclude',
-    'count': '10'
-  }
+class Get:
+  #Available form types [0-Q, 10-K,8-K, 20-F, 40-F, 6-K]
+  #SEC implementation
+  @staticmethod
+  def CIK(company_name):
+    params = {
+      'action': 'getcompany',
+      'company': company_name,
+      'owner': 'exclude',
+      'count': '10'
+    }
+      
+    response = requests.get(search_url, params=params, headers=usr_agnt)
     
-  response = requests.get(search_url, params=params, headers=usr_agnt)
-    
-  if response.status_code == 200:
-    soup = BeautifulSoup(response.text, 'html.parser')
-    cik_tag = soup.find('input', {'name': 'CIK'})
-    if cik_tag:
-      return cik_tag['value']
+    if response.status_code == 200:
+      soup = BeautifulSoup(response.text, 'html.parser')
+      cik_tag = soup.find('input', {'name': 'CIK'})
+      if cik_tag:
+        return cik_tag['value']
+      else:
+        return "Company not found or CIK not available."
     else:
-      return "Company not found or CIK not available."
-  else:
-    print("Error fetching data:", response.status_code)
-    return None  
+      print("Error fetching data:", response.status_code)
+      return None  
 
-def Get_10k(cik):
-  params = {
-    'action': 'getcompany',
-    'owner': 'exclude',
-    'count': '10',
-    'CIK': cik
-  }
+  @staticmethod
+  def Json(cik):
+    params = {
+      'action': 'getcompany',
+      'owner': 'exclude',
+      'count': '10',
+      'CIK': cik
+    }
 
-  response = requests.get(search_url, params=params, headers=usr_agnt)
+    response = requests.get(f'https://data.sec.gov/submissions/CIK{cik}.json', params=params, headers=usr_agnt)
 
-  if response.status_code == 200:
-    soup = BeautifulSoup(response.text, 'html.parser')
-    filings = soup.find_all('a', string='10-K')  # Find all links with '10-K' text
+    if response.status_code == 200:
+      #print(response.json())
+      return response.json()
+  def tenk(json):
+    Print('try')
 
-    if filings:
-      # Take the first 10-K filing link
-      filing_link = filings[0]['href']
-      full_filing_url = 'https://www.sec.gov' + filing_link
-      return full_filing_url
-    else:
-      return f"10-K form not found for CIK {cik}"
-  else:
-    print("Error fetching data:", response.status_code)
-    return None
+num = str(Get.CIK('Apple Inc.'))
+#print(GetJson.Get_URL(num))
+content = Get.Json(num)
 
-print(Get_CIK('Apple inc.'))
-print(Get_10k(Get_CIK('Apple inc.')))
+with open("output.json", "w") as file: 
+  json.dump(content, file, indent=4)
