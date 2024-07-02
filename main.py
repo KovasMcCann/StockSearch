151f0362f0286f09bf35ee11be72a6f850eb4d98
KovasMcCann <KovasMcCann@outlook.com>
2024-07-02 13:02:56 -0500

    implemented redis for cik

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
 import json
-#header for SEC access
+import redis 
 
+#redis config 
+redis_host = '10.1.10.131'
+redis_port = 6379
+redis_db = 0  # default database
+redis_password = None  # no password set
+r = redis.StrictRedis(host=redis_host, port=redis_port, db=redis_db, password=redis_password)
+
+#header for SEC access
 usr_agnt = {'User-Agent' : 'Kovas McCann (KovasMcCann@outlook.com)'}
 Polygon_API_KEY = ''
 search_url = "https://www.sec.gov/cgi-bin/browse-edgar"
+
+
 class Get:
   #Available form types [0-Q, 10-K,8-K, 20-F, 40-F, 6-K]
   #SEC implementation
@@ -47,7 +57,8 @@ class Get:
       return response.json()
   def tenk(json):
     Print('try')
-  
+
+# implement redis
 class Parse:
   def tenk(json):
     print('try')
@@ -57,7 +68,8 @@ class Parse:
     print('try')
   def fourteena(json):
     print('try')
-  
+
+"""
 num = str(Get.CIK('Apple Inc.'))
 #print(GetJson.Get_URL(num))
 #print(Get.CIK('Apple Inc.'))
@@ -65,13 +77,48 @@ content = Get.Json(num)
 
 with open("output.json", "w") as file: 
   json.dump(content, file, indent=4)
+"""
 
-""" Propere output.josn links to the SEC website for the company's filings.
+## Redis Example
+"""
+r.select(0) #select database 0
+
+r.set('foo', 'bar')
+value = r.get('foo')
+print(value.decode('utf-8'))
+
+r.select(1) #select database 1  
+
+r.set('second', 'bars')
+value = r.get('second')
+print(value.decode('utf-8'))
+"""
+
+url = 'https://www.sec.gov/include/ticker.txt'
+table = requests.get(url, headers=usr_agnt)
+if table.status_code == 200:
+    stored = table.text.split()
+    # Switch to database 0 once instead of doing it in the loop
+    r.select(0)
+    # Use a pipeline to batch commands
+    pipeline = r.pipeline()
+    for i in range(0, len(stored), 2):
+        pipeline.set(stored[i], stored[i+1])
+    # Execute all commands in the batch
+    pipeline.execute()
+    value = r.get('aapl')
+    print(value.decode('utf-8'))
+else:
+    print('Error fetching data:', table.status_code)
+
+""" 
+Propere output.josn links to the SEC website for the company's filings.
 https://www.sec.gov/Archives/edgar/data/xslF345X05/0000320193-24-000075-index.html
 to get this link you need to look at the:
 primaryDocument - xslF345X05
 accessionNumber - 0000320193-24-000075
 
-
-
+Redis Database config 
+database 0 - for ticker and CIK
+database 1 - for the filing url
 """
