8d9b15642c870edaf00f115cfee3a6301451cb9e
KovasMcCann <KovasMcCann@outlook.com>
2024-08-24 14:01:32 -0500

    working

--- DataBaseBuilders/datastore.py
+++ DataBaseBuilders/datastore.py
@@ -280,6 +280,61 @@ for ticker in tickers:
     #write_ticker(ticker.decode('utf-8'))
     buildarray(ticker.decode('utf-8'))
 """
+from datetime import datetime
+import time
+
+# Define holidays and non-working days
+holidays = [
+    (1, 1),  # January 1
+    (1, 15), # January 15 (MLK Day varies)
+    (2, 19), # February 19 Washington Birthday varies)
+    (3, 29), # March 29 (Good Friday varies)
+    (5, 27), # May 27 (Memorial Day varies)
+    (6, 19), # June 19 (Juneteenth)
+    (9, 2),  # September 2 (Labor Day varies)
+    (7, 4),  # July 4
+    (11, 28),# November 28 (Thanksgiving varies)
+    (12, 25) # December 25
+]
+
+def is_holiday(date):
+    return (date.month, date.day) in holidays
+def time_range(current_time): 
+    return current_time < 800 or current_time > 1459 # for chicago time
+    #return current_time < 900 or current_time > 1559 # for new york time
+
+def is_weekend(date):
+    return date >= 5
+
+def next_minute():
+    now = datetime.now()
+    current = now.strftime("%S")
+    return 60 - int(current)
+
+def main():
+    while True:
+        now = datetime.now()
+        day = now.weekday()
+        #day = int(6)
+        current_time = int(now.strftime("%H%M"))
+        display_time = now.strftime("%H:%M %m/%d/%Y")
+
+        if is_holiday(now):
+            print(f'[{display_time}] {RED}Market Closed: Holiday{RESET}') #need to add partial holidays
+        elif is_weekend(day):
+            print(f'[{display_time}] {RED}Market Closed: Weekend{RESET}')
+        elif time_range(current_time):
+            print(f'[{display_time}] {YELLOW}Market Closed{RESET}')
+        else:
+            print(f'[{display_time}] {GREEN}Market Open{RESET}')
+            #Start storing data
+
+        # Sleep to prevent excessive CPU usage
+        time.sleep(next_minute())  # Check every minute
+
+if __name__ == "__main__":
+    main()
+
 
 import concurrent.futures
 import os
@@ -289,8 +344,9 @@ def process_ticker(ticker):
     ticker_str = ticker.decode('utf-8')
     print(ticker_str)
     # Uncomment the next line if you need to write the ticker
-    # write_ticker(ticker_str)
-    buildarray(ticker_str)
+    if main() == "Open":
+        write_ticker(ticker_str)
+        #buildarray(ticker_str)
 
 def main():
     tickers = r.keys('*')
